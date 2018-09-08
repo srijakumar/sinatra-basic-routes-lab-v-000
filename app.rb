10095d510352b2bafd0e39f06cc8e592f0cdddfb
Srija S <srijakumar7@gmail.com>
2018-09-08 23:45:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is srija"
+  end
+
+  get '/hometown' do
+    "My hometown is DC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Havana"
+  end
+
+
 end
